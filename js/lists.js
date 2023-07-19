d7eb5be408007a6660272aadbe95de9e54399c99
snahn ip <are.zee.dee@gmail.com>
2023-07-19 03:33:40 -0700

    more cleanups

--- js/lists.js
+++ js/lists.js
@@ -84,16 +84,16 @@
       ]
   
       let numberList = [
-        'One',
-        'Two',
-        'Three',
-        'Four',
-        'Five',
-        'Six',
-        'Seven',
-        'Eight',
-        'Nine',
-        'Ten',
+        '1-One',
+        '2-Two',
+        '3-Three',
+        '4-Four',
+        '5-Five',
+        '6-Six',
+        '7-Seven',
+        '8-Eight',
+        '9-Nine',
+        '10-Ten',
         'CONGRATULATIONS'
       ]
   
